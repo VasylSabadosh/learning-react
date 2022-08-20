e2fdb23c4af9dc5159ce1764f3afa6d2b0389e8d
Vasyl <vasia.sabadosh@gmail.com>
2022-08-20 16:58:56 +0300

    added new component counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import MyFirstComponet from './components/MyFirstComponent/MyFirstComponet';
 import Header from "./components/Header/Header";
+import Counter from './components/Counter/Counter';
 
 
 function App() {
+  let names = ['Vasyl','Vasyl', 'Anna', 'Anna', 'Boris', 'Tetiana', 'Igor'];
   return (
     <div className="App">
       <Header title = "learning react"/>
-      <MyFirstComponet message = "1task-2"/>
-      <MyFirstComponet message = "2task-2"/>
-      <MyFirstComponet message = "3task-2"/>
-      <MyFirstComponet message = "4task-2"/>
-
+      <MyFirstComponet arr = {names}/>
+      <Counter />
     </div>
   );
 }
--- src/components/MyFirstComponent/MyFirstComponet.jsx
+++ src/components/MyFirstComponent/MyFirstComponet.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import "./MyFirstComponet.css";
+import { v4 as uuidv4 } from 'uuid';
 
 class MyFirstComponet extends Component {
+    state = {
+        a:this.props.arr,
+
+    }
 
     render() {
         return (
             <div className='first'>
-                <div>{this.props.message}</div>
+                <div>
+                    {this.state.a
+                        .filter((item, index) => {
+                        return this.state.a.indexOf(item) === index
+                        })
+                        .map( name => <p key={uuidv4()}>{name}</p> )}
+                </div>
             </div>
         );
     }
